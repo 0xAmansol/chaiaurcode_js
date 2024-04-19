7a616b5bddc2da207156dd9383863bd3177c8582
Aman Kalal <84617672+thecrazyengineerguy@users.noreply.github.com>
2024-04-19 17:27:39 +0000

    datatypes summary

---
+++ basics-01/04_datatypesSummary.js
@@ -0,0 +1,41 @@
+// Primitive datatypes: Number, boolean, Bigint, string , null, undefined, Symbol.
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// datatypes and what they return in typeof method:
+/* Number : Number
+   String : String
+   BigInt : BigInt
+   All the non primitive datatypes return typeof as functions and functions return as object functions*/
+
